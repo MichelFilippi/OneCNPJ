52d5a6e60240a80e2627b13493dce94aa7530032
Michel Filippi <michel.filippidealmeida@gmail.com>
2025-12-15 15:01:10 -0300

    Retirando os Program.cs e alteração no CnaeVO

--- OneCNPJ/OneCNPJ.DTOs/VOs/Satelites/CnaeVO.cs
+++ OneCNPJ/OneCNPJ.DTOs/VOs/Satelites/CnaeVO.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OneCNPJ.Domain.Models.Satelites;
+using OneCNPJ.DTOs.VOs.Satelites.Listas;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,36 @@ using System.Threading.Tasks;
 
 namespace OneCNPJ.DTOs.VOs.Satelites
 {
-    public class CnaeVO :
-        CnaeVO, IEntityListVO
+    public class CnaeVO
+           : BaseVO,
+             IEntityVO<Cnae, CnaeVO, CnaeListaVO>
     {
+        public string Descricao { get; set; } = string.Empty;
+
+        public CnaeVO() { }
+
+        public CnaeVO FromDomain(Cnae model)
+        {
+            return new CnaeVO
+            {
+                Descricao = model.Descricao
+            };
+        }
+
+        public CnaeListaVO ListFromDomain(Cnae model)
+        {
+            return new CnaeListaVO
+            {
+                Descricao = model.Descricao
+            };
+        }
+
+        public Cnae ToDomain()
+        {
+            return new Cnae
+            {
+                Descricao = this.Descricao
+            };
+        }
     }
 }
